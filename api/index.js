bcc6f1977b0590cea95f2f66628677ab317b3688
JayFiji <fkafkaandthem@gmail.com>
2023-12-15 19:34:50 -0300

    creacion de test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(console.log('conectado a mongoDB'))
@@ -9,9 +10,13 @@ mongoose.connect(process.env.MONGO).then(console.log('conectado a mongoDB'))
 
 const app = express();
 
+app.use(express.json());
+
+
 app.listen(3000, () => {
     console.log('Server corriendo en puerto 3000!!!');
 });
 
 //creación api route
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
