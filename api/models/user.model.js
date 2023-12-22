ec122c181679b4ea924a84f1833e266ef3f3bb2c
JayFiji <fkafkaandthem@gmail.com>
2023-12-22 00:02:10 -0300

    Actualizado el header y la página profile pasa a ser privada

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,11 +17,13 @@ const userSchema = new mongoose.Schema({
     },
     isAdmin: {
         type: Boolean,
-        default: false
+        default: false,
+        required:true,
     },
     avatar:{
         type: String,
-        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        required:true,
     }
 },
 {timestamps:true});
