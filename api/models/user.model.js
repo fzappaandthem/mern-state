3e01077e1fea95146201f1b12024cc8f6305d029
JayFiji <fkafkaandthem@gmail.com>
2023-12-24 17:23:32 -0300

    Completa la carga de imagen desde la página de perfil privada

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,11 +15,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-        required:true,
-    },
     avatar:{
         type: String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -21,7 +21,7 @@ export default function OAuth() {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    name: result.user.displayName, email: result.user.email, photo: result.user.photoURL, isAdmin: result.user.isAdmin,
+                    name: result.user.displayName, email: result.user.email, photo: result.user.photoURL
                 }),
             });
             const data = await res.json();
