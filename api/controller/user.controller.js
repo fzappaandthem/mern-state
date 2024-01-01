430faac5d0a6d63c746d45d53932394d809dd0f7
JayFiji <fkafkaandthem@gmail.com>
2023-12-31 21:48:07 -0300

    Creación del Route API para el listado de usuario

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,12 +1,26 @@
 import {errorHandler} from "../utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import Listing from "../models/listing.model.js";
 
 export const test=(req, res) => {
     res.json({
         message: "api route is working",
     })
 };
+
+export const getUserListings= async (req, res, next)=>{
+    if(req.user.id===req.params.id){
+        try {
+            const listings = await Listing.find({userRef: req.params.id});
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    }else{
+        return next(errorHandler(401, 'Sólo podes ver tus propias publicaciones'));
+    }
+}
 
 export const updateUser = async (req, res, next) => {
     if(req.user.id !== req.params.id) return next(errorHandler(401, "Sólo podes modificar tu propia cuenta"));
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { test, updateUser } from "../controller/user.controller.js";
 import { verifyToken } from "../utils/verifiUser.js";
-import { deleteUser } from "../controller/user.controller.js";
+import { deleteUser, getUserListings } from "../controller/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
+
+
 
 export default router;
