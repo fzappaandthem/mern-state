1f44a71fc199ffbc674760e7ae50bbb089a960a5
JayFiji <fkafkaandthem@gmail.com>
2024-01-03 16:44:06 -0300

    Completo getlistings api route para obtener publicaciones desde la busqueda

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing } from "../controller/listing.controller.js";
+import { createListing, deleteListing, getListings } from "../controller/listing.controller.js";
 import { verifyToken } from "../utils/verifiUser.js";
 import { getListing, updateListing } from "../controller/listing.controller.js";
 
@@ -9,5 +9,6 @@ router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 export default router;
