62f3113d6f8f109e19f57ec16f85c3ff683f9701
JayFiji <fkafkaandthem@gmail.com>
2024-01-06 17:00:37 -0300

    Cambio en el header ahora se llama FrancescaPropiedades

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,8 +28,8 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
               <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                  <span className='text-slate-500'>Poblador</span>
-                  <span className='text-slate-700'>Rural</span>
+                  <span className='text-slate-500'>Francesca</span>
+                  <span className='text-slate-700'>Propiedades</span>
               </h1>
             </Link>
             <form onSubmit={handleSubmit} className='bg-slate-100 p3 rounded-lg flex items-center'>
