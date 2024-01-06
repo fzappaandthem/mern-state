a7cad5c9969da1e8e7bc5c3f073f45691da2e7f8
JayFiji <fkafkaandthem@gmail.com>
2024-01-06 17:28:57 -0300

    Completa la página Nosotros

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-6xl mx-auto'>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Acerca de FrancescaPropiedades</h1>
+      <p className='mb-4 text-slate-700'>Soy Gastón Di Filippo, un programador independiente que está buscando abrirse
+        paso en el mundo del software, dejando atrás un trabajo aburrido. En este caso 
+        creé una aplicación utilizando varias tecnologías. Este es un modelo de aplicación
+        que puedo trabajar para usar como base en todo tipo de productos. Me encanta crear
+        soluciones para los problemas de mis clientes.
+      </p>
+      <p className='mb-4 text-slate-700'>Mi misión es ayudar a clientes a  classNameobtener una mejor experiencia, a 
+        medida que van aplicando tecnologías diversas para resolver problemas con los
+        que se enfrentan todos los días. De esta manera, se llega a más clientes y por
+        ende, el mismo obtiene más beneficios económicos. El producto software es cómodo y fácil,
+        creado a la medida de las necesidades de cada cliente.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        A medida que mi modelo de negocios vaya creciendo y la cartera de clientes aumente,
+        será necesario brindar trabajo de calidad a otros/as programadores/as que formen
+        parte de este microemprendimiento.
+      </p>
+    </div>
   )
 }
